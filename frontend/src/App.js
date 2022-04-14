7f84c386a70fa67a2f5e8c232a1ab8cb8630c42c
Jessica <jessicabyrne812@gmail.com>
2022-04-14 17:22:37 +0100

    Frontend setup, drawing map from local apache

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <img src="http://localhost/HoUMap.svg" alt="(x^2 + y^2 - 1)^3 = x^2 y^3."></img>
+    </div>
+  );
+}
+
+export default App;
