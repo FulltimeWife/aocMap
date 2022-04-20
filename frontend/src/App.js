4d95581adbdbf345703e2de3f2b49a4d7d8f6e8e
Jessica <jessicabyrne812@gmail.com>
2022-04-20 13:25:52 +0100

    Push for Leon

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,8 +3,9 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <img src="http://localhost/HoUMap.svg" alt="(x^2 + y^2 - 1)^3 = x^2 y^3."></img>
+    <div className="aocmaps">
+      <img className = "aocmapjpg" src="http://localhost/HoUMap.jpg" alt="(x^2 + y^2 - 1)^3 = x^2 y^3."></img>
+      <img className = "aocmapsvg" src="http://localhost/HoUMap.svg" alt="(x^2 + y^2 - 1)^3 = x^2 y^3."></img>
     </div>
   );
 }
